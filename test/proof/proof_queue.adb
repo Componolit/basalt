dcd23625041535a5c93f4d78f74285cc773fc107
Johannes Kliemann <kliemann@componolit.com>
2019-11-19 17:16:04 +0100

    remove obsolete comments

--- test/proof/proof_queue.adb
+++ test/proof/proof_queue.adb
@@ -14,14 +14,12 @@ is
    begin
       Fifo.Initialize (Queue, 0);
       for I in Integer range 7 .. 13 loop
-         --  Componolit/Workarounds#2
          Fifo.Put (Queue, I);
          exit when Fifo.Count (Queue) >= Fifo.Size (Queue);
       end loop;
       pragma Assert (Fifo.Count (Queue) = 7);
       for I in Integer range 1 .. 7 loop
          Fifo.Pop (Queue, J);
-         --  Componolit/Workarounds#2
       end loop;
    end Prove;
 
