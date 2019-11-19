3379e5142a47b0b3235c78f4c8ed19e66b8bc532
Johannes Kliemann <kliemann@componolit.com>
2019-11-19 14:45:41 +0100

    add unit tests
    ref #3

---
+++ test/aunit/suite.adb
@@ -0,0 +1,24 @@
+
+with Basalt.Strings.Tests;
+with Queue_Tests;
+with Slicer_Tests;
+
+package body Suite
+is
+
+   Result : aliased Aunit.Test_Suites.Test_Suite;
+
+   Strings_Case : aliased Basalt.Strings.Tests.Test_Case;
+   Queue_Case    : aliased Queue_Tests.Test_Case;
+   Slicer_Case  : aliased Slicer_Tests.Test_Case;
+
+   function Suite return Aunit.Test_Suites.Access_Test_Suite
+   is
+   begin
+      Result.Add_Test (Strings_Case'Access);
+      Result.Add_Test (Queue_Case'Access);
+      Result.Add_Test (Slicer_Case'Access);
+      return Result'Access;
+   end Suite;
+
+end Suite;
