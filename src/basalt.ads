e81023eb2473986d1172d28229de018105377a2b
Johannes Kliemann <kliemann@componolit.com>
2019-11-19 13:45:39 +0100

    add strings, slicer and queue
    fixes #2

---
+++ src/basalt.ads
@@ -0,0 +1,14 @@
+--
+--  @summary Basalt
+--  @author  Johannes Kliemann
+--  @date    2019-11-19
+--
+--  Copyright (C) 2019 Componolit GmbH
+--
+--  This file is part of Basalt, which is distributed under the terms of the
+--  GNU Affero General Public License version 3.
+--
+
+package Basalt is
+
+end Basalt;
