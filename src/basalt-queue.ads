1b69e0512508347585bc64ed15695d7649cb41cd
Johannes Kliemann <kliemann@componolit.com>
2019-11-19 16:40:33 +0100

    add doc comments
    ref #4

--- src/basalt-queue.ads
+++ src/basalt-queue.ads
@@ -9,40 +9,86 @@
 --  GNU Affero General Public License version 3.
 --
 
+--  Basalt.Queue is a generic fifo queue implementation. It can use elements
+--  of any non-limited type.
+
 generic
    type T is private;
 package Basalt.Queue with
    SPARK_Mode
 is
 
+   --  Queue storage type
+   --
+   --  As the queue elements are stored in this type
+   --  its objects can become considerably large for a
+   --  large type T or large queue sizes.
+   --
+   --  @param Size  Length of the queue
    type Queue (Size : Positive) is private;
 
+   --  Checks if a queue is valid, proof only
+   --
+   --  @param Q  Queue
+   --  @return   Q is valid
    function Valid (Q : Queue) return Boolean with
       Ghost;
 
+   --  Returns the size of the queue given when instantiated
+   --
+   --  @param Q  Queue
+   --  @return   Number of Elements the queue can hold
    function Size (Q : Queue) return Positive with
       Pre => Valid (Q);
 
+   --  Returns the current length of the queue
+   --
+   --  @param Q  Queue
+   --  @return   Number of elements currently in the queue
    function Count (Q : Queue) return Natural with
       Pre => Valid (Q);
 
+   --  Initializes the queue with a default element value
+   --
+   --  This procedure is only needed to proof the data flow. It is
+   --  the only way to assign a Queue object. The object will automatically
+   --  be initialized with the given element. The initialized queue will be empty.
+   --
+   --  @param Q             Queue
+   --  @param Null_Element  Default element to initialize the queue with
    procedure Initialize (Q            : out Queue;
                          Null_Element :     T) with
       Post => Valid (Q) and then Count (Q) = 0;
 
+   --  Puts an element in the queue.
+   --
+   --  @param Q        Queue
+   --  @param Element  Element
    procedure Put (Q       : in out Queue;
                   Element :        T) with
       Pre  => Valid (Q) and then Count (Q) < Size (Q),
       Post => Valid (Q) and then Count (Q) = Count (Q'Old) + 1;
 
+   --  Check the current first element, does not alter the queue
+   --
+   --  @param Q        Queue
+   --  @param Element  Head of the queue
    procedure Peek (Q       :     Queue;
                    Element : out T) with
       Pre => Valid (Q) and then Count (Q) > 0;
 
+   --  Drop the current first element
+   --
+   --  @param Q  Queue
    procedure Drop (Q : in out Queue) with
       Pre  => Valid (Q) and then Count (Q) > 0,
       Post => Valid (Q) and then Count (Q) = Count (Q'Old) - 1;
 
+   --  Get the first element of the queue and drop it,
+   --  equivalent to subsequent calls to Peek and Drop
+   --
+   --  @param Q        Queue
+   --  @param Element  First element of the queue, will be dropped
    procedure Pop (Q       : in out Queue;
                   Element :    out T) with
       Pre  => Valid (Q) and then Count (Q) > 0,
--- src/basalt-slicer.ads
+++ src/basalt-slicer.ads
@@ -9,20 +9,36 @@
 --  GNU Affero General Public License version 3.
 --
 
+--  Basalt.Slicer is a generic slicing tool that allows to
+--  split a range into multiple subsequent slices of a maximum length.
+--  All slices will be equal to the given maximum length. If the
+--  range cannot be divided into full slices the last slice will be
+--  smaller.
+
 generic
    type Index is range <>;
 package Basalt.Slicer with
    SPARK_Mode
 is
 
+   --  Slicer context
    type Context is private;
 
+   --  Slice that denotes the range First .. Last
+   --  @field First  First element of the slice
+   --  @field Last   Last element of the slice
    type Slice is record
       First : Index;
       Last  : Index;
    end record with
       Dynamic_Predicate => Slice.First <= Slice.Last;
 
+   --  Create a slicer
+   --
+   --  @param Range_First   First element of the range to be sliced
+   --  @param Range_Last    Last element of the range to be sliced
+   --  @param Slice_Length  Maximum length of a single slice
+   --  @return              Context
    function Create (Range_First  : Index;
                     Range_Last   : Index;
                     Slice_Length : Index) return Context with
@@ -32,17 +48,37 @@ is
               and then Get_Range (Create'Result).Last = Range_Last
               and then Get_Length (Create'Result) = Slice_Length;
 
+   --  Get the current slice
+   --
+   --  @param C  Context
+   --  @return   The current slice that is selected
    function Get_Slice (C : Context) return Slice with
       Post => Get_Slice'Result.Last - Get_Slice'Result.First < Get_Length (C)
               and then Get_Slice'Result.First in Get_Range (C).First .. Get_Range (C).Last
               and then Get_Slice'Result.Last in Get_Range (C).First .. Get_Range (C).Last;
 
+   --  Get the configured range
+   --
+   --  @param C  Context
+   --  @return   The range that has been configured in Create
    function Get_Range (C : Context) return Slice;
 
+   --  Get the configured length
+   --
+   --  @param C  Context
+   --  @return   The length that has been configured in Create
    function Get_Length (C : Context) return Index;
 
+   --  Check if a further slice is available
+   --
+   --  @param C  Context
+   --  @return   True if a new slice is available,
+   --            False if the current slice is the last one
    function Has_Next (C : Context) return Boolean;
 
+   --  Iterate to the next slice
+   --
+   --  @param C  Context
    procedure Next (C : in out Context) with
       Pre  => Has_Next (C),
       Post => Get_Range (C'Old) = Get_Range (C)
