585146285815ae802c2d1147e9eb63f433fb9b91
Alexander Senier <senier@componolit.com>
2019-12-02 00:41:07 +0100

    Add stack proof

---
+++ test/proof/proof_stack.adb
@@ -0,0 +1,26 @@
+
+with Basalt.Stack;
+
+package body Proof_Stack with
+   SPARK_Mode
+is
+
+   package Stack is new Basalt.Stack (Integer);
+   S : Stack.Stack_Type (10);
+
+   procedure Prove
+   is
+   begin
+      Stack.Initialize (S, 0);
+      for I in Integer range 7 .. 13 loop
+         Stack.Push (S, I);
+         exit when Stack.Count (S) >= Stack.Size (S);
+      end loop;
+      pragma Assert (Stack.Count (S) = 7);
+      for I in Integer range 1 .. 7 loop
+         Stack.Drop (S);
+      end loop;
+      pragma Assert (Stack.Is_Empty (S));
+   end Prove;
+
+end Proof_Stack;
---
+++ test/proof/proof_stack.ads
@@ -0,0 +1,8 @@
+
+package Proof_Stack with
+   SPARK_Mode
+is
+
+   procedure Prove;
+
+end Proof_Stack;
