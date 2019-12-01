fab053e8a6949cf56b565259c4c72ece3155ff34
Alexander Senier <senier@componolit.com>
2019-12-02 00:27:30 +0100

    Migrate stack from SXML
    Ref. #6, Componolit/SXML#69

--- test/aunit/suite.adb
+++ test/aunit/suite.adb
@@ -2,6 +2,7 @@
 with Basalt.Strings.Tests;
 with Queue_Tests;
 with Slicer_Tests;
+with Stack_Tests;
 
 package body Suite
 is
@@ -9,8 +10,9 @@ is
    Result : aliased Aunit.Test_Suites.Test_Suite;
 
    Strings_Case : aliased Basalt.Strings.Tests.Test_Case;
-   Queue_Case    : aliased Queue_Tests.Test_Case;
+   Queue_Case   : aliased Queue_Tests.Test_Case;
    Slicer_Case  : aliased Slicer_Tests.Test_Case;
+   Stack_Case   : aliased Stack_Tests.Test_Case;
 
    function Suite return Aunit.Test_Suites.Access_Test_Suite
    is
@@ -18,6 +20,7 @@ is
       Result.Add_Test (Strings_Case'Access);
       Result.Add_Test (Queue_Case'Access);
       Result.Add_Test (Slicer_Case'Access);
+      Result.Add_Test (Stack_Case'Access);
       return Result'Access;
    end Suite;
 
