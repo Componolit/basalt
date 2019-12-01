0b9cad1c569f25a9ce53e64522ca3f5c7dd9a2ef
Alexander Senier <senier@componolit.com>
2019-12-02 00:27:30 +0100

    Migrate stack from SXML
    Ref. #6, Componolit/SXML#69

---
+++ test/aunit/stack_tests.adb
@@ -0,0 +1,122 @@
+with Aunit.Assertions;
+with Basalt.Stack;
+
+package body Stack_Tests
+is
+   package F is new Basalt.Stack (Integer);
+
+   procedure Test_Stack (T : in out Aunit.Test_Cases.Test_Case'Class)
+   is
+      pragma Unreferenced (T);
+      Q : F.Stack_Type (100);
+      J : Integer;
+   begin
+      F.Initialize (Q, 0);
+      for I in 70 .. 120 loop
+         F.Push (Q, I);
+      end loop;
+      for I in reverse 70 .. 120 loop
+         F.Pop (Q, J);
+         Aunit.Assertions.Assert (J = I, "Invalid order");
+      end loop;
+   end Test_Stack;
+
+   procedure Test_Full_Empty (T : in out Aunit.Test_Cases.Test_Case'Class)
+   is
+      pragma Unreferenced (T);
+      Q : F.Stack_Type (2);
+   begin
+      F.Initialize (Q, 0);
+      Aunit.Assertions.Assert (F.Count (Q) = 0, "Count not 0");
+      F.Push (Q, 1);
+      Aunit.Assertions.Assert (F.Count (Q) = 1, "Count not 1 after Push");
+      F.Push (Q, 2);
+      Aunit.Assertions.Assert (F.Count (Q) = 2, "Count not 2 after Push");
+      F.Drop (Q);
+      Aunit.Assertions.Assert (F.Count (Q) = 1, "Count not 1 after Drop");
+      F.Drop (Q);
+      Aunit.Assertions.Assert (F.Count (Q) = 0, "Count not 0 after Drop");
+   end Test_Full_Empty;
+
+   procedure Test_Single_Element (T : in out Aunit.Test_Cases.Test_Case'Class)
+   is
+      pragma Unreferenced (T);
+      Q : F.Stack_Type (1);
+      J : Integer;
+   begin
+      F.Initialize (Q, 0);
+      Aunit.Assertions.Assert (F.Is_Empty (Q), "Stack not empty");
+      Aunit.Assertions.Assert (F.Count (Q) = 0, "Stack not empty");
+      F.Push (Q, 1);
+      Aunit.Assertions.Assert (F.Count (Q) = 1, "Stack not full");
+      Aunit.Assertions.Assert (F.Is_Full (Q), "Stack not full");
+      F.Drop (Q);
+      Aunit.Assertions.Assert (F.Is_Empty (Q), "Stack not empty");
+      Aunit.Assertions.Assert (F.Count (Q) = 0, "Stack not empty after drop");
+   end Test_Single_Element;
+
+   procedure Test_Count (T : in out Aunit.Test_Cases.Test_Case'Class)
+   is
+      pragma Unreferenced (T);
+      Q : F.Stack_Type (100);
+   begin
+      F.Initialize (Q, 0);
+      Aunit.Assertions.Assert (F.Count (Q) = 0, "Count should be 0");
+      for I in Integer range 1 .. 20 loop
+         F.Push (Q, I);
+      end loop;
+      Aunit.Assertions.Assert (F.Count (Q) = 20, "Count should be 20");
+      for I in Integer range 1 .. 40 loop
+         F.Push (Q, I);
+      end loop;
+      Aunit.Assertions.Assert (F.Count (Q) = 60, "Count should be 60");
+      for I in Integer range 1 .. 50 loop
+         F.Drop (Q);
+      end loop;
+      Aunit.Assertions.Assert (F.Count (Q) = 10, "Count should be 10");
+      for I in Integer range 1 .. 80 loop
+         F.Push (Q, I);
+      end loop;
+      Aunit.Assertions.Assert (F.Count (Q) = 90, "Count should be 90");
+      for I in Integer range 1 .. 90 loop
+         F.Drop (Q);
+      end loop;
+      Aunit.Assertions.Assert (F.Count (Q) = 0, "Count should be 0");
+   end Test_Count;
+
+   procedure Test_Size (T : in out Aunit.Test_Cases.Test_Case'Class)
+   is
+      pragma Unreferenced (T);
+      Q1 : F.Stack_Type (1);
+      Q2 : F.Stack_Type (50);
+      Q3 : F.Stack_Type (200);
+      Q4 : F.Stack_Type (13000);
+   begin
+      F.Initialize (Q1, 0);
+      F.Initialize (Q2, 0);
+      F.Initialize (Q3, 0);
+      F.Initialize (Q4, 0);
+      Aunit.Assertions.Assert (F.Size (Q1) = 1, "Size of Q1 should be 1");
+      Aunit.Assertions.Assert (F.Size (Q2) = 50, "Size of Q2 should be 50");
+      Aunit.Assertions.Assert (F.Size (Q3) = 200, "Size of Q3 should be 200");
+      Aunit.Assertions.Assert (F.Size (Q4) = 13000, "Size of Q4 should be 13000");
+   end Test_Size;
+
+   procedure Register_Tests (T : in out Test_Case)
+   is
+      use Aunit.Test_Cases.Registration;
+   begin
+      Register_Routine (T, Test_Stack'Access, "Test stack");
+      Register_Routine (T, Test_Full_Empty'Access, "Test full and emtpy");
+      Register_Routine (T, Test_Single_Element'Access, "Test single element");
+      Register_Routine (T, Test_Count'Access, "Test count");
+      Register_Routine (T, Test_Size'Access, "Test size");
+   end Register_Tests;
+
+   function Name (T : Test_Case) return Aunit.Message_String
+   is
+   begin
+      return Aunit.Format ("Basalt.Stack");
+   end Name;
+
+end Stack_Tests;
--- test/aunit/test.adb
+++ test/aunit/test.adb
@@ -10,8 +10,10 @@ is
    use type Aunit.Status;
    function Run is new Aunit.Run.Test_Runner_With_Status (Suite.Suite);
    Reporter : Aunit.Reporter.Text.Text_Reporter;
-   S        : Aunit.Status := Run (Reporter);
+   S        : Aunit.Status;
 begin
+   Reporter.Set_Use_ANSI_Colors (True);
+   S := Run (Reporter);
    if S = Aunit.Success then
       Ada.Command_Line.Set_Exit_Status (Ada.Command_Line.Success);
    else
